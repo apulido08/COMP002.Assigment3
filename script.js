60c3fc5529665c8c7667fb8ab63c7120a394ccd7
adodoCoder <alavropulido@whccd.edu>
2024-11-03 16:25:43 -0800

    added circumference function

--- script.js
+++ script.js
@@ -14,7 +14,10 @@
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
-        
+        function calculateCircumference(radius){
+            return 2 * 3.14 * radius;
+        }
+        console.log(calculateCircumference(5))
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
